fc17375b331973c59ed680495fc20656a9a43004
keshavgituser <kesparad@gmail.com>
2021-10-30 11:25:04 +0530

    Done with registration

--- src/app/user_components/registerform/registerform.component.ts
+++ src/app/user_components/registerform/registerform.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { RegisterService } from 'src/app/services/register.service';
+import { LoginformComponent } from '../loginform/loginform.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-registerform',
@@ -8,29 +11,56 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class RegisterformComponent implements OnInit {
 
+  private urole:string="dealer";
+
+  
+
   registerForm={
-    id:null,
-    role:'',
-    Username:'',
-    Loginname:'',
+    
+    userName:'',
+    loginName:'',
     pwd:'',
-    phoneno:''
+    phone:'',
+    role:`${this.urole}`,
+   
   }
+  private registersuccess:boolean=false;
+
+  constructor(private register:RegisterService,private snak:MatSnackBar){ }
 
   ngOnInit(): void {
+    
   }
 
   hide:boolean = true;
 
+  
+
   submitForm(){
     console.log("Form submitted");
-    //console.log("DAta ",this.form.value);
+    console.log("DAta ",this.registerForm);
+
     
+    
+    this.register.registerUser(this.urole,this.registerForm).subscribe(
+      response=>{
+
+        console.log(response);
+        this.registersuccess=true;
+        
+      },
+      error=>{
+          console.log(error);
+          this.registersuccess=true;
+          
+      }
+    )
+
+
+    if(this.registersuccess==true)
+    {
+      this.snak.open("Registered Successfully");
+    }
   }
 
-  
-  // saveForm(){
-  //   console.log('Form data is',this.profileForm.value);
-    
-  // }
 }
