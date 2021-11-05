94d420471dc78faa000972b1b12e9418c86926a5
keshavgituser <kesparad@gmail.com>
2021-11-05 18:33:24 +0530

    Working on table

--- src/app/adservice_components/viewadstable/viewadstable.component.ts
+++ src/app/adservice_components/viewadstable/viewadstable.component.ts
@@ -1,9 +1,15 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
+import { MatTable, _MatTableDataSource } from '@angular/material/table';
 import { AdvertiseService } from 'src/app/services/advertise.service';
-import { ViewadstableDataSource, ViewadstableItem } from './viewadstable-datasource';
+// import { ViewadDataSource } from './viewadstable-datasource';
+// import { ViewadItem, ViewadDataSource, ViewadstableItem } from './viewadstable-datasource';
+import { ViewadItem } from './viewad-item';
+import { DataSource } from '@angular/cdk/collections';
+
+
+
 
 @Component({
   selector: 'app-viewadstable',
@@ -11,22 +17,37 @@ import { ViewadstableDataSource, ViewadstableItem } from './viewadstable-datasou
   styleUrls: ['./viewadstable.component.css']
 })
 export class ViewadstableComponent implements AfterViewInit {
+  AD_DATA: ViewadItem[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<ViewadstableItem>;
-  dataSource: ViewadstableDataSource;
+  @ViewChild(MatTable) table!: MatTable<ViewadItem>;
+  dataSource=new _MatTableDataSource<ViewadItem>(this.AD_DATA);
+  // dataSource=ViewadDataSource;
+
+  
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ["advertiseId","advertiseIdentifier","availableStock","offerDescription","postedBy"];
 
+
+  ad_data={  
+    
+    // title:'',
+    // advertiseIdentifier:'',
+    // offerDescription:'',
+    // availableStock:'',
+    // postedBy:'',
+  }
   constructor(private adservice:AdvertiseService) {
-    this.dataSource = new ViewadstableDataSource();
+    
   }
 
   ngAfterViewInit(): void {
+
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
+
   }
 
   ngOnInit(): void {
@@ -37,20 +58,11 @@ export class ViewadstableComponent implements AfterViewInit {
 
   getAllAdvertisements(){
 
-    this.adservice.getAllAdvertisements().subscribe(
+    let resp=this.adservice.getAllAdvertisements();
 
-      response=>{
-        console.log(response);
-        
-      }
-      ,
-      error=>{
-        console.log(error);
-        
+    resp.subscribe(ad=>this.dataSource.data=ad as ViewadItem[])
 
-
-      }
-    )
+      
 
 
   }
